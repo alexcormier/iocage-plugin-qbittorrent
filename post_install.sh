e7f6e0de2cbf35f3eb9dbe758c0778befbec1c6f
Alexandre Cormier <alexandre@cormier.io>
2021-03-13 19:26:59 -0500

    initial commit

---
+++ post_install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+source /root/vars
+
+# make sure qBittorrent only uses the wireguard interface
+sed -i '' 's/^Connection\\Interface=.*$/Connection\\Interface='${WG_IFACE}'/' "${CONFIG}"
+sed -i '' 's/^Connection\\InterfaceName=.*$/Connection\\InterfaceName=Wireguard/' "${CONFIG}"
+
+sysrc wireguard_enable="YES"
+sysrc wireguard_interfaces="${WG_IFACE}"
+
+sysrc qbittorrent_enable="YES"
+
+/usr/local/bin/plugin start
+
+echo "Default credentials: admin/adminadmin" >> /root/PLUGIN_INFO
+echo "Place your wireguard config at ${wireguard_config}" >> /root/PLUGIN_INFO
